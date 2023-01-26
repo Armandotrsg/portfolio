7a2917bc9f8b76c57e689262219ae650816c94a6
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-01-26 13:30:09 -0600

    Contact Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Navigate,
   Route,
@@ -18,6 +20,8 @@ function App() {
         <NavBar />
         <Banner />
         <Skills />
+        <Projects />
+        <Contact />
       </div>
     </BrowserRouter>
   );
