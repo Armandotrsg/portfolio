7ef50271addb7b5eba3dd36a0e984a4c66cf2b92
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-01-27 12:03:47 -0600

    Fixed overflow in the writing animation at the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -46,27 +46,27 @@ export const Banner = () => {
       }
 
     return (
-        <section className="banner" id="home">
+        <header className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
+                    <Col xs={12} md={7} xl={8}>
                         <TrackVisibility>
                           {({isVisible}) =>
                           <div className={isVisible ? "animated_animated animate_fadeIn" : ""}>
                             <span className="tagline">Hi There!</span>
-                            <div className='fixed-height'>
-                              <h1>{`I'm Armando, a `} <span className="txt-rotate"><span className={isDeleting ? "writing" : "wrap"}>{text}</span></span></h1>
-                            </div>
+                                  <div className='fixed-height'>
+                                    <h1>I'm Armando a <span className="txt-rotate"><span className={isDeleting ? "writing" : "wrap"}>{text}</span></span></h1>
+                                  </div>
                             <p>Example Text</p>
                             <button role="link" className="vvd">Let's connect <ArrowRightCircle size={25} /> </button>
                           </div> }
                         </TrackVisibility>
                     </Col>
-                    <Col xs={12} md={6} xl={5}>
+                    <Col xs={12} md={5} xl={4}>
                         <img src={'https://raw.githubusercontent.com/judygab/web-dev-projects/main/personal-portfolio/src/assets/img/header-img.svg'} alt="banner" />
                     </Col>
                 </Row>
             </Container>
-        </section>
+        </header>
     );
 }
